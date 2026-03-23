441aaa71131c253f3fb8bce15c18064b332f4c6e
davidtiger3622 <davidwafula3622@gmail.com>
2026-03-23 14:02:38 -0400

    Task 5: Add submit and show_exam_result views and update urls

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
 app_name = 'onlinecourse'
 urlpatterns = [
     # route is a string contains a URL pattern
@@ -16,9 +15,6 @@ urlpatterns = [
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
-
-    # <HINT> Create a route for submit view
-
-    # <HINT> Create a route for show_exam_result view
-
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='exam_result'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
